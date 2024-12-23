fa63197728258fdf3e17c483806735a21169bd7e
Abdelaziz HAIMOUD <abdelazizhaimoud@outlook.com>
2024-12-23 22:47:05 +0100

    add sign up icon

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -47,7 +47,7 @@
         <div class="row border rounded-5 p-3 bg-white shadow box-area">
             <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box">
                 <div class="featured-image mb-3">
-                    <img src="{{asset('images/Signup.png')}}" class="img-fluid" style="width: 600px;">
+                    <img src="{{asset('images/inscription.png')}}" class="img-fluid" style="width: 600px;">
                 </div>
                 <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Join Us!</p>
                 <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Be part of our amazing community by creating your account now.</small>
